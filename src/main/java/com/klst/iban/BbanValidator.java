8837f4bb192fb29ffd6ac20fd7da4e4c18ac5270
homebeaver <eugen.hanussek@klst.com>
2020-08-23 16:21:37 +0200

    BBAN structure MC to XK

--- src/main/java/com/klst/iban/BbanValidator.java
+++ src/main/java/com/klst/iban/BbanValidator.java
@@ -53,7 +53,7 @@ public class BbanValidator {
     	BBAN_DATA.put("IS", new BbanValidator("(\\d{4})(\\d{18})"                  )); // 4!n2!n6!n10!n Kontrollzeichen+account+sonstige
     	BBAN_DATA.put("IT", new BbanValidator("([A-Z]{1})(\\d{5})(\\d{5})([A-Z0-9]{12})", 2, 1)); // 1!a5!n5!n12!c Kontrollzeichen+BankCode+BranchCode+account
     	BBAN_DATA.put("JO", new BbanValidator("([A-Z]{4})(\\d{4})([A-Z0-9]{18})", 1)); // 4!a4!n18!c +BranchCode (nicht bei iban.com)
-    	BBAN_DATA.put("KW", new BbanValidator("([A-Z]{4})([A-Z0-9]{22})"        , 1)); // 4!a22!c +BranchCode
+    	BBAN_DATA.put("KW", new BbanValidator("([A-Z]{4})([A-Z0-9]{22})"           )); // 4!a22!c
     	BBAN_DATA.put("KZ", new BbanValidator("(\\d{3})([A-Z0-9]{13})"             )); // 3!n13!c
     	BBAN_DATA.put("LB", new BbanValidator("(\\d{4})([A-Z0-9]{20})"             )); // 4!n20!c
     	BBAN_DATA.put("LC", new BbanValidator("([A-Z]{4})([A-Z0-9]{24})"           )); // 4!a24!c
@@ -61,8 +61,37 @@ public class BbanValidator {
     	BBAN_DATA.put("LT", new BbanValidator("(\\d{5})(\\d{11})"                  )); // 5!n11!n
     	BBAN_DATA.put("LU", new BbanValidator("(\\d{3})([A-Z0-9]{13})"             )); // 3!n13!c
     	BBAN_DATA.put("LV", new BbanValidator("([A-Z]{4})([A-Z0-9]{13})"           )); // 4!a13!c
-    	// ...
-    	// "XK"
+    	BBAN_DATA.put("MC", new BbanValidator("(\\d{5})(\\d{5})([A-Z0-9]{13})"  , 1)); // 5!n5!n11!c2!n +BranchCode account+Kontrollzeichen
+    	BBAN_DATA.put("MD", new BbanValidator("([A-Z0-9]{2})([A-Z0-9]{18})"        )); // 2!c18!c  	
+    	BBAN_DATA.put("ME", new BbanValidator("(\\d{3})(\\d{15})"                  )); // 3!n13!n2!n account+Kontrollzeichen (Großbuchstabe oder Ziffer)
+       	BBAN_DATA.put("MK", new BbanValidator("(\\d{3})([A-Z0-9]{12})"             )); // 3!n10!c2!n
+    	BBAN_DATA.put("MR", new BbanValidator("(\\d{5})(\\d{5})(\\d{13})"       , 1)); // 5!n5!n11!n2!n +BranchCode account+Kontrollzeichen (Großbuchstabe oder Ziffer)
+    	BBAN_DATA.put("MT", new BbanValidator("([A-Z]{4})(\\d{5})([A-Z0-9]{18})", 1)); // 4!a5!n18!c + BranchCode
+    	BBAN_DATA.put("MU", new BbanValidator("([A-Z]{4})(\\d{2})([A-Z0-9]{20})", 1)); // 4!a2!n2!n12!n3!n3!a +BranchCode account+Kontrollzeichen (Großbuchstabe oder Ziffer)
+    	BBAN_DATA.put("NL", new BbanValidator("([A-Z]{4})(\\d{10})"                )); // 4!a10!n
+    	BBAN_DATA.put("NO", new BbanValidator("(\\d{4})(\\d{7})"                   )); // 4!n6!n1!n
+    	BBAN_DATA.put("PK", new BbanValidator("([A-Z]{4})([A-Z0-9]{16})"           )); // 4!a16!c
+    	BBAN_DATA.put("PL", new BbanValidator("(\\d{8})(\\d{16})"                  )); // 8!n16!n
+    	BBAN_DATA.put("PS", new BbanValidator("([A-Z]{4})([A-Z0-9]{21})"           )); // 4!a21!c
+    	BBAN_DATA.put("PT", new BbanValidator("(\\d{4})(\\d{4})(\\d{13})"       , 1)); // 4!n4!n11!n2!n +BranchCode account+Kontrollzeichen (Großbuchstabe oder Ziffer)
+    	BBAN_DATA.put("QA", new BbanValidator("([A-Z]{4})([A-Z0-9]{21})"           )); // 4!a21!c
+    	BBAN_DATA.put("RO", new BbanValidator("([A-Z]{4})([A-Z0-9]{16})"           )); // 4!a16!c
+    	BBAN_DATA.put("RS", new BbanValidator("(\\d{3})(\\d{15})"                  )); // 3!n13!n2!n
+    	BBAN_DATA.put("SA", new BbanValidator("(\\d{2})([A-Z0-9]{18})"             )); // 2!n18!c
+    	BBAN_DATA.put("SC", new BbanValidator("([A-Z]{4})([A-Z0-9]{23})"           )); // 4!a2!n2!n16!n3!a BankCode:4!a2!n2!n iban.com nur 4!a account+Kontrollzeichen+sonstige
+    	BBAN_DATA.put("SE", new BbanValidator("(\\d{3})(\\d{17})"                  )); // 3!n16!n1!n
+    	BBAN_DATA.put("SI", new BbanValidator("(\\d{5})(\\d{10})"                  )); // 5!n8!n2!n
+    	BBAN_DATA.put("SK", new BbanValidator("(\\d{4})(\\d{16})"                  )); // 4!n6!n10!n
+    	BBAN_DATA.put("SM", new BbanValidator("([A-Z]{1})(\\d{5})(\\d{5})([A-Z0-9]{12})", 2, 1)); // 1!a5!n5!n12!c Kontrollzeichen+BankCode+BranchCode+account
+    	BBAN_DATA.put("ST", new BbanValidator("(\\d{4})(\\d{4})(\\d{13})"       , 1)); // 8!n11!n2!n +BranchCode account+Kontrollzeichen (Großbuchstabe oder Ziffer)
+    	BBAN_DATA.put("SV", new BbanValidator("([A-Z]{4})(\\d{20})"                )); // 4!a20!n
+    	BBAN_DATA.put("TL", new BbanValidator("(\\d{3})(\\d{16})"                  )); // 3!n14!n2!n
+    	BBAN_DATA.put("TN", new BbanValidator("(\\d{2})(\\d{3})(\\d{15})"       , 1)); // 2!n3!n13!n2!n +BranchCode account+Kontrollzeichen
+    	BBAN_DATA.put("TR", new BbanValidator("(\\d{5})(\\d{17})"                  )); // 5!n1!n16!c Kontrollzeichen+account
+    	BBAN_DATA.put("UA", new BbanValidator("(\\d{6})([A-Z0-9]{19})"             )); // 6!n19!c
+    	BBAN_DATA.put("VA", new BbanValidator("(\\d{3})(\\d{15})"                  )); // 3!n15!n
+    	BBAN_DATA.put("VG", new BbanValidator("([A-Z]{4})(\\d{16})"                )); // 4!a16!n
+    	BBAN_DATA.put("XK", new BbanValidator("(\\d{2})(\\d{2})(\\d{12})"       , 1)); // 4!n10!n2!n +BranchCode account+Kontrollzeichen
     }
     
     static final int GROUP_BANK_IDENTIFIER = 0;
