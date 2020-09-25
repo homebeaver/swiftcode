51b939d59098fb47b88719097011fb3e97952cb0
homebeaver <eugen.hanussek@klst.com>
2020-09-25 20:39:22 +0200

    BankDataGenerator MK/RS and SE

--- src/main/java/com/klst/iban/BankDataGenerator_SE.java
+++ src/main/java/com/klst/iban/BankDataGenerator_SE.java
@@ -9,11 +9,11 @@ import org.json.simple.JSONObject;
 
 import com.klst.iban.Result.BankData;
 
-public class BankDataGenerator_MK extends BankDataGenerator {
+public class BankDataGenerator_SE extends BankDataGenerator {
 
-	private static final Logger LOG = Logger.getLogger(BankDataGenerator_MK.class.getName());
+	private static final Logger LOG = Logger.getLogger(BankDataGenerator_SE.class.getName());
 	
-	BankDataGenerator_MK(String api_key) {
+	BankDataGenerator_SE(String api_key) {
 		super(api_key);
 	}
 
@@ -27,9 +27,6 @@ public class BankDataGenerator_MK extends BankDataGenerator {
 			Object cityAlt = jo.get(key);
 			if(!city.equals(cityAlt) && listIndex>0) {
 				//LOG.warning("JSONObject:"+jo + " - diff in "+key+":"+city);
-				if(jo.get(BRANCH)==null && city.length()==0) {
-					jo = updateJSONObject(jo, BRANCH, "branch "+cityAlt);
-				}
 			}
 			jo = updateJSONObject(jo, ADDRESS, bankData.getAddress());
 			jo = updateJSONObject(jo, ZIP, bankData.getZipString());
@@ -57,12 +54,9 @@ public class BankDataGenerator_MK extends BankDataGenerator {
 	}
 	
 	public static void main(String[] args) throws Exception {
-		BankDataGenerator test = new BankDataGenerator_MK("testKey");
+		BankDataGenerator test = new BankDataGenerator_SE("testKey");
 
-//		test.tryWith("MK", BankDataGenerator.FORMAT_03d, 000, 999, "120000058984"); // MK07 250 120000058984
-		// Serbia gleiches Muster:
-//		test.tryWith("RS", BankDataGenerator.FORMAT_03d, 000, 999, "005601001611379"); // RS35 260 005601001611379
+		test.tryWith("SE", BankDataGenerator.FORMAT_03d, 000, 999, "00000058398257466"); // SE45 500 00000058398257466
 		
-		//test.tryWith("SA", BankDataGenerator.FORMAT_02d, 00, 99, "000000608010167519"); // SA03 80 000000608010167519
 	}
 }
